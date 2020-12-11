546d0b259fcc95314ed1cdbe820987d1df1494ca
shilaranidas <shilarani.das@georgebrown.ca>
2020-12-11 11:19:35 -0500

    add collision manager

---
+++ Assets/_Scripts/CubeBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Color = UnityEngine.Color;
+
+[System.Serializable]
+public class CubeBehaviour : MonoBehaviour
+{
+    public Vector3 size;
+    public Vector3 max;
+    public Vector3 min;
+    public bool isColliding;
+    public List<CubeBehaviour> contacts;
+
+    private MeshFilter meshFilter;
+    private Bounds bounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+
+        bounds = meshFilter.mesh.bounds;
+        size = bounds.size;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        max = Vector3.Scale(bounds.max, transform.localScale) + transform.position;
+        min = Vector3.Scale(bounds.min, transform.localScale) + transform.position;
+    }
+    
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+
+        Gizmos.DrawWireCube(transform.position, Vector3.Scale(new Vector3(1.0f, 1.0f, 1.0f), transform.localScale));   
+    }
+
+}
