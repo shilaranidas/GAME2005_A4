4d5b7520848a5b676cb5345ea4ed6fa50af881ef
shilaranidas <shilarani.das@georgebrown.ca>
2020-12-14 13:05:13 -0500

    add button image and start simulation

---
+++ Assets/_Scripts/ButtonHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartSimulation()
+    {
+     Debug.Log("start simulation")   ;
+    }
+
+   
+}
